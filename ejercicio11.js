e3f31811a12d5d221c74adf78ddbd8adb3d5b30b
davidson124 <daferdinat@gmail.com>
2025-12-12 21:14:07 -0500

    Solución ejercicio 11. Uso de for of, setTimeout

--- ejercicio11.js
+++ ejercicio11.js
@@ -1,16 +1,27 @@
-// Completa el siguiente programa e imprime la cantidad de unos (1) que haya en el arreglo:
+// 11. Completa el siguiente programa e imprime la cantidad de unos (1) que haya en el arreglo:
 
+//     `const nums = [ 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1 ];`
 
+//     El resultado debería ser el siguiente:
 
-// El resultado debería ser el siguiente:
+//     ``` JavaScript
+//     11
+//     ```
 
-// 11
-// NOTA: este ejercicio lo debes hacer con ciclos y sin ayuda de la función filter de JavaScript. Al terminarlo, cambia el arreglo original para verificar que funcione con otras combinaciones de ceros y unos.
+//     NOTA: este ejercicio lo debes hacer con ciclos y sin ayuda de la función filter de JavaScript. Al terminarlo, cambia el arreglo original para verificar que funcione con otras combinaciones de ceros y unos.
+
 const nums = [ 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1 ];
-let total = 0;
-for(let k of nums){
-    if ( k == 1 ){
-        total += k;
+alert(`De la siguiente lista ${nums}`);
+
+alert('Hallaremos la cantidad de unos (1)');
+
+let numUno = 0
+
+for(let d of nums ){
+    if (d == 1){
+        numUno+=d;
     }
 }
-alert (total);
+setTimeout(()=>{
+    console.log('La respuesta es :',numUno);
+},800)
